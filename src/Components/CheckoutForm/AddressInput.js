15f936cf33520334db242a7e8a3fa4a89d164a83
Josimar-Victoria <josimarvictoria968@gmail.com>
2021-08-24 13:42:47 -0500

    modificaciones de mi ecomerse en el chekout y el herder

--- src/Components/CheckoutForm/AddressInput.js
+++ src/Components/CheckoutForm/AddressInput.js
@@ -1,10 +1,12 @@
-import { Button } from '@material-ui/core';
+import { Button, makeStyles } from '@material-ui/core';
 import { Grid} from '@material-ui/core'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom';
 import { actionTypes } from '../../reducer';
 import { useStateValue } from '../../StateProvide';
+
 const AddressInput = ({nextStep}) => {
+    const classes = useStyles();
     const [{shippingData}, dispatch] = useStateValue()
 
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
@@ -22,13 +24,13 @@ const AddressInput = ({nextStep}) => {
     return (
         <div>
             <Grid item xs={12} sm={6} >
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <input defaultValue="" {...register("firsName", { required: true })} placeholder="firsName" />
-                <input defaultValue="" {...register("lastName", { required: true })} placeholder="lastName" />
-                <input defaultValue="" {...register("addressl", { required: true })} placeholder="addressl" />
-                <input defaultValue="" {...register("email", { required: true })} placeholder="email" />
-                <input defaultValue="" {...register("city", { required: true })} placeholder="city" />
-                <input {...register("postCode", { required: true })} placeholder="postCode"  />
+            <form onSubmit={handleSubmit(onSubmit)} >
+                <input className={classes.form} defaultValue="" {...register("firsName", { required: true })} placeholder="firsName" />
+                <input className={classes.form} defaultValue="" {...register("lastName", { required: true })} placeholder="lastName" />
+                <input className={classes.form} defaultValue="" {...register("addressl", { required: true })} placeholder="addressl" />
+                <input className={classes.form} defaultValue="" {...register("email", { required: true })} placeholder="email" />
+                <input className={classes.form} defaultValue="" {...register("city", { required: true })} placeholder="city" />
+                <input className={classes.form} {...register("postCode", { required: true })} placeholder="postCode"  />
                 {errors.exampleRequired && <span>This field is required</span>}
                 <div style={{display: 'flex', justifyContent:'space-between',marginTop:'1rem'}}>
                     <Button component={Link} to="/checkout-page">Back to the Checkout Pages</Button>
@@ -42,3 +44,11 @@ const AddressInput = ({nextStep}) => {
 }
 
 export default AddressInput
+
+const useStyles = makeStyles((theme) => ({
+    form:{
+      display: 'flex',
+      justifyContent: 'center', 
+    
+    } 
+}))
--- src/Components/CheckoutForm/PaymentForm.js
+++ src/Components/CheckoutForm/PaymentForm.js
@@ -1,6 +1,8 @@
 import { Divider } from '@material-ui/core'
 import React from 'react'
 import Review from './Review'
+
+
 const PaymentForm = ({backStep, nextStep}) => {
   return (
     <>
